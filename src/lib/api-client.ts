0de1fed5a0979b3f1815876f47ea9f33f188f92f
aahhmmeeddaammr <aamr24987@gmail.com>
2026-04-26 19:39:30 +0300

    fix login

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -1,14 +1,14 @@
 import axios from 'axios';
 import { useAuthStore } from '@/stores/authStore';
 
-// const apiClient = axios.create({
-//   baseURL: 'https://noor-einak-backend-production.up.railway.app/api/v1',
-//   headers: { 'Content-Type': 'application/json' },
-// });
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://noor-einak-backend-production.up.railway.app/api/v1',
   headers: { 'Content-Type': 'application/json' },
 });
+// const apiClient = axios.create({
+//   baseURL: 'http://localhost:3000/api/v1',
+//   headers: { 'Content-Type': 'application/json' },
+// });
 
 apiClient.interceptors.request.use((config) => {
   const { accessToken } = useAuthStore.getState();
